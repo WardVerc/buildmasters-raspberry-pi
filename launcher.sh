82cf9e6703b071ecd6774ca09346e3c162487381
WardVerc <ward.vercruyssen@hotmail.be>
2024-11-14 12:16:46 +0100

    update launcher script

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-
+echo cding now
 cd /home/pi/buildmasters-raspberry-pi
+echo pulling from github now
 git pull
+echo npm installing now
 npm install
+echo npm run dev now
 npm run dev
