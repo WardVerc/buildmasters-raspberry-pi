83bcf8fc43a885bce2d25f80ebaa190b56bedff1
WardVerc <ward.vercruyssen@hotmail.be>
2024-11-28 20:00:49 +0100

    feat: free up ports 3000, 4000 on boot

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,10 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+# Kill processes using ports 3000 and 4000
+lsof -ti:3000 | xargs kill -9
+lsof -ti:4000 | xargs kill -9
+
 echo "Changing directory"
 cd /home/pi/buildmasters-raspberry-pi || exit 1
 
