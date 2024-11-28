db71e299e3effc13558d8736ffad085ce86d60e7
WardVerc <ward.vercruyssen@hotmail.be>
2024-11-28 21:00:32 +0100

    fix: adjust sh scripts

--- launcher.sh
+++ launcher.sh
@@ -5,10 +5,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-# Kill processes using ports 3000 and 4000
-lsof -ti:3000 | xargs kill -9
-lsof -ti:4000 | xargs kill -9
-
 echo "Changing directory"
 cd /home/pi/buildmasters-raspberry-pi || exit 1
 
--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 # shutdown.sh
 
-# Kill processes using ports 3000 and 4000
-lsof -ti:3000 | xargs kill -9
-lsof -ti:4000 | xargs kill -9
+# Stop React and Python processes gracefully
+echo "Stopping processes on ports 3000 and 4000..."
 
+# Use `pkill` to stop Vite React and Python processes by name
+pkill -f "vite"    # Vite React app
+pkill -f "python"  # Python backend
+
+# Give processes some time to terminate
+sleep 5
+
+# Confirm the ports are freed
+if lsof -ti:3000 || lsof -ti:4000; then
+    echo "Forcing processes to terminate..."
+    lsof -ti:3000 | xargs kill -9
+    lsof -ti:4000 | xargs kill -9
+else
+    echo "Ports 3000 and 4000 are free."
+fi
+
+# Reboot the Raspberry Pi
+echo "Rebooting the system..."
 sudo reboot
